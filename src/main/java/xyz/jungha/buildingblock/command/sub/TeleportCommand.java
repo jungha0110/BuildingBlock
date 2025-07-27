84fcb1d8b222470a1c455836c2e5126302057613
정하 <sjh0521a@gmail.com>
2025-07-28 00:37:03 +0900

    BuildBlock 1.0.0

--- src/main/java/xyz/jungha/buildingblock/command/sub/TeleportCommand.java
+++ src/main/java/xyz/jungha/buildingblock/command/sub/TeleportCommand.java
@@ -14,23 +14,16 @@ public class TeleportCommand extends AbstractSubCommand {
     @Override
     public boolean execute(CommandSender sender, String[] args) {
         Player player = getPlayer(sender);
-        if (player == null) {
-            return true;
-        }
+        if (player == null) return true;
 
-        if (args.length != 1) {
+        if (args.length != 2) {
+            sender.sendMessage(MINI_MESSAGE.deserialize(":red_ex: <#f9cccc>잘못된 ID 형식입니다."));
             return false;
         }
 
-        String[] id = args[0].split("-");
-        if (id.length != 2) {
-            sender.sendMessage(MINI_MESSAGE.deserialize(":red_ex: <#f9cccc>잘못된 ID 형식입니다."));
-            return true;
-        }
-
         try {
-            int chunkX = Integer.parseInt(id[0]);
-            int chunkZ = Integer.parseInt(id[1]);
+            int chunkX = Integer.parseInt(args[0]);
+            int chunkZ = Integer.parseInt(args[1]);
             int blockX = (chunkX << 4) + 8;
             int blockZ = (chunkZ << 4) + 8;
             int blockY = player.getWorld().getHighestBlockYAt(blockX, blockZ);
